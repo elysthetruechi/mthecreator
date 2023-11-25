00ffe8753bd8e4618881205567b20b74186858e7
elysthetruechi <kaluelizabeth29@gmail.com>
2023-11-25 15:32:05 +0100

    .

--- src/components/programs.jsx
+++ src/components/programs.jsx
@@ -5,10 +5,7 @@ const programs = () => {
   return (
     <section className="programs">
         <div className="container programs__container">
-            <div className="programs__head">
-                <span>Brand Identity Design Projects</span>
-                <div class="nutrition"></div>
-            </div>
+            
         </div>
     </section>
   )
---
+++ src/components/sectionHead.jsx
@@ -0,0 +1,11 @@
+
+
+const sectionHead = ({title, className}) => {
+  return (
+    <div className="section__head">
+    <h2>{title}</h2>
+</div>
+  )
+}
+
+export default sectionHead
