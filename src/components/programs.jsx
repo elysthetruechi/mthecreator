ba45cfdff6c6f3c1f7cf673231a1928140d17268
elysthetruechi <kaluelizabeth29@gmail.com>
2023-11-25 22:40:22 +0100

    .

--- src/components/programs.jsx
+++ src/components/programs.jsx
@@ -1,7 +1,7 @@
 import SectionHead from "./sectionHead"
 import {programs} from "../data"
 import Card from "../UI/card"
-import Image from './Images/ArryTRpresentation_page-0013 1.png'
+
 
 const Programs = () => {
   return (
@@ -14,7 +14,7 @@ const Programs = () => {
             programs.map(({id, title, image, path}) =>{
               return (
                 <Card className= "programs__program" key={id}>
-                  <span>{Image}</span>
+                  <span>{image}</span>
                    <h3>{title}</h3>
                 </Card>
               )
--- src/data.js
+++ src/data.js
@@ -36,24 +36,24 @@ export const programs = [
     id: 1,
     title: "BRAND IDENTITY DESIGN FOR HOMEFEEL ENTERPRISES",
     path: "/programs/111",
-    Image: "./components/Images/Bucket Hat Mockupjk 1.png",
+    image: "./components/Images/Bucket Hat Mockupjk 1.png",
     },
     {
         id: 2,
         title: "BRAND IDENTITY DESIGN FOR PERSEVERANCE&RESILLIANCE",
         path: "/programs/222",
-        Image: "./components/Images/Free_Logo_Mockup_3ss 1.png"
+        image: "./components/Images/Free_Logo_Mockup_3ss 1.png"
         },
         {
             id: 3,
-            title: "BRAND IDENTITY FOR THE HELLIFINO",
+            title: "BRAND IDENTITY FOR HELLIFINO",
             path: "/programs/333",
-            Image: "./components/Images/Landscape_DL_Brochure_Mockup_5t 1.png"
+            image: "./components/Images/Landscape_DL_Brochure_Mockup_5t 1.png"
             },
             {
                 id: 4,
                 title: "BRAND IDENTITY FOR ARRY'S TASTE RIDE",
                 path: "/programs/444",
-                Image: "./components/Images/ArryTRpresentation_page-0013 1.png"
+                image: "./components/Images/ArryTRpresentation_page-0013 1.png"
                 },   
 ]
